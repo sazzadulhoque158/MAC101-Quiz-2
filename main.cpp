1e1e861404de77513bd2b3b739f1e571f464e25f
Hessvacio Hassan <madeinbrooklyngames@gmail.com>
2025-10-21 17:11:31 -0400

    Initialize main.cpp with FizzBuzz TODOs
    Add main function skeleton with TODOs for FizzBuzz.

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    TODO: Create int var and cout and cin commands
+
+
+    // TODO: Write a for loop that runs from 1 to limit
+    
+
+        // TODO: Use if/else statements to apply FizzBuzz logic
+
+    
+
+    return 0;
+}
