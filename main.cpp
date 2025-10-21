f6b38da4c4181c55159aa10339fe8c3911cd57af
sazzadulhoque158 <sazzadulhoque158@gmail.com>
2025-10-21 19:31:50 -0400

    Implement FizzBuzz logic with user input

--- main.cpp
+++ main.cpp
@@ -2,15 +2,31 @@
 using namespace std;
 
 int main() {
-    //TODO: Create int var and cout and cin commands
+    int num;
+    bool seen3 = false;
+    bool seen5 = false;
 
+    while (true) {
+        cout << "Enter a number (0 to quit): ";
+        cin >> num;
 
-    // TODO: Write a for loop that runs from 1 to limit
-    
+        if (num == 0) break;
 
-        // TODO: Use if/else statements to apply FizzBuzz logic
+        // Track if 3 or 5 has been entered
+        if (num == 3) seen3 = true;
+        if (num == 5) seen5 = true;
 
-    
+        if (seen3 && seen5) {
+            cout << "FizzBuzz" << endl;
+        } else if (num == 3) {
+            cout << "Fizz" << endl;
+        } else if (num == 5) {
+            cout << "Buzz" << endl;
+        } else {
+            cout << num << endl;
+        }
+    }
 
+    cout << "Program ended." << endl;
     return 0;
 }
